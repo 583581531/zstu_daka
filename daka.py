006ce14cfaec582f1da71aa564b9431991849780
583581531 <52961366+583581531@users.noreply.github.com>
2021-01-23 13:28:02 +0800

    Update daka.py

--- daka.py
+++ daka.py
@@ -58,8 +58,6 @@ def login():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--start-maximized')
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
     driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
